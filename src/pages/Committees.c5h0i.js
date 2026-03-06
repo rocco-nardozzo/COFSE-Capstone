97f2a1e8166e0bd37b1755ef3e0b246920273c5a
Rocco Nardozzo <nardozzor@duq.edu>
2026-03-06 00:10:36 -0500

    Set up base membership page and fixed some styling

--- src/pages/Committees.c5h0i.js
+++ src/pages/Committees.c5h0i.js
@@ -10,31 +10,51 @@ $w.onReady(function () {
 	$w("#committees-header").html = `<p style="text-align: center;font-size: 42px;font-family: 'Oswald', sans-serif;">${committees}</p>`;
 
 	//Linking membership and outreach header to the appropriate page
+	const membership = $w("#membership-link").text;
+    $w("#membership-link").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold; text-decoration: underline;">${membership}</p>`;
+
 	$w("#membership-link").onClick(() => {
         wixLocation.to("/membership-and-outreach-committee"); 
     });
 
 	//Linking higher education header to the appropriate page
+	const higher = $w("#higher-edu-link").text;
+    $w("#higher-edu-link").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold; text-decoration: underline;">${higher}</p>`;
+
 	$w("#higher-edu-link").onClick(() => {
         wixLocation.to("/higher-education-committee"); 
     });
 
 	////Linking K-12 header to the appropriate page
+
+	const k_12 = $w("#k-12-link").text;
+    $w("#k-12-link").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold; text-decoration: underline;">${k_12}</p>`;
+
 	$w("#k-12-link").onClick(() => {
         wixLocation.to("/k-12-education-committee"); 
     });
 
 	//Linking research header to the appropriate page
+
+	const research = $w("#research-link").text;
+    $w("#research-link").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold; text-decoration: underline;">${research}</p>`;
+
 	$w("#research-link").onClick(() => {
         wixLocation.to("/research-committee"); 
     });
 
 	//Linking nominating header to the appropriate page
+	const nominating = $w("#nominating-committee").text;
+    $w("#nominating-committee").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold; text-decoration: underline;">${nominating}</p>`;
+
 	$w("#nominating-committee").onClick(() => {
         wixLocation.to("/nomination-committee"); 
     });
 
 	//Linking DDE header to the appropriate page
+	const DDE = $w("#DDE-link").text;
+    $w("#DDE-link").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold; text-decoration: underline;">${DDE}</p>`;
+
 	$w("#DDE-link").onClick(() => {
         wixLocation.to("/dde-committee"); 
     });
--- src/pages/Membership.fabev.js
+++ src/pages/Membership.fabev.js
@@ -1,10 +1,33 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+
 
 $w.onReady(function () {
-    // Write your JavaScript here
+    const colorBlue = "#87aade";
 
-    // To select an element by ID use: $w('#elementID')
+    const membership = $w("#Membership-header").text;
+	$w("#Membership-header").html = `<p style="text-align: center;font-size: 42px;font-family: 'Oswald', sans-serif;">${membership}</p>`;
 
-    // Click 'Preview' to run your code
+    const full = $w("#full-member").text;
+    $w("#full-member").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold;">${full}</p>`;
+
+    const associate = $w("#associate-member").text;
+    $w("#associate-member").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold;">${associate}</p>`;
+
+    const retired = $w("#retired-member").text;
+    $w("#retired-member").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold;">${retired}</p>`;
+
+    const honorary = $w("#honorary-member").text;
+    $w("#honorary-member").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold;">${honorary}</p>`;
+
+    const sponsoring = $w("#sponsoring-member").text;
+    $w("#sponsoring-member").html = `<p style="text-align: center;font-size: 20px;font-family: 'Oswald', sans-serif; font-weight: bold;">${sponsoring}</p>`;
+
+    const apply_link = `<a href="/membership-application" target="_self" style="color: ${colorBlue}; font-weight: bold;">here</a>`;
+    let apply = $w("#apply").html;
+    apply = apply.split("{here}").join(apply_link);
+    $w("#apply").html = apply;
+
+    const login_link = `<a href="/member-login" target="_self" style="color: ${colorBlue}; font-weight: bold;">here</a>`;
+    let login = $w("#login").html;
+    login = login.split("{here}").join(login_link);
+    $w("#login").html = login;
 });
