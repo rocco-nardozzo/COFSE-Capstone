81f651293e08826df53fa2e04a3a805729c38746
Rocco Nardozzo <nardozzor@duq.edu>
2026-03-23 00:12:35 -0400

    Annual Meetings Page

--- src/pages/Member Resources.ojrk6.js
+++ src/pages/Member Resources.ojrk6.js
@@ -1,15 +1,32 @@
 // Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+import wixLocation from 'wix-location';
 
 $w.onReady(function () {
 
-	// Write your Javascipt code here using the Velo framework API
+	const resources = $w("#resources-header").text;
+	$w("#resources-header").html = `<p style="text-align: center;font-size: 42px;font-family: 'Oswald', sans-serif;">${resources}</p>`;
 
-	// Print hello world:
-	// console.log("Hello world!");
+	$w("#meetings").onClick(() => {
+		wixLocation.to("/annual-meetings");
+	});
 
-	// Call functions on page elements, e.g.:
-	// $w("#button1").label = "Click me!";
+	$w("#presentations").onClick(() => {
+		wixLocation.to("/presentations");
+	});
 
-	// Click "Run", or Preview your site, to execute your code
+	$w("#workshops").onClick(() => {
+		wixLocation.to("/workshops");
+	});
 
+	$w("#directory").onClick(() => {
+		wixLocation.to("/member-directory");
+	});
+
+	$w("#additional-resources").onClick(() => {
+		wixLocation.to("/additional-resources");
+	});
+
+	$w("#dues").onClick(() => {
+		wixLocation.to("/membership-dues");
+	});
 });
