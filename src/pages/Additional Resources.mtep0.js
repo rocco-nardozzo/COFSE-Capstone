f3877311da3c252a3fcc09e3de0df2a8d99279a4
Rocco Nardozzo <nardozzor@duq.edu>
2026-03-23 23:42:53 -0400

    Presentation and Additional Resources Links

--- src/pages/Additional Resources.mtep0.js
+++ src/pages/Additional Resources.mtep0.js
@@ -2,9 +2,17 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    // Write your JavaScript here
+    const colorBlue = "#87aade";
 
-    // To select an element by ID use: $w('#elementID')
+    const teaching_resources = $w("#teaching-resources").text;
+    $w("#teaching-resources").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_ca85e61c7586467a9fcb54a3606829ec.pdf?online-teaching-resources.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${teaching_resources}</a></p>`;
 
-    // Click 'Preview' to run your code
+    const labs = $w("#labs").text;
+    $w("#labs").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_47cd51f5e7ef4568bdec74db8158a70e.pdf?forensic-labs-for-high-school-teachers.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${labs}</a></p>`;
+
+    const programs = $w("#program-list").text;
+    $w("#program-list").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_313b69cc480f4e729cfca0fef87c46fa.xlsx?us-forensic-science-programs-feb-2019.xlsx" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${programs}</a></p>`;
+
+    const ascld = $w("#ascld").text;
+    $w("#ascld").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_81d7f8f83a434456b9a8d09e34cbb4b0.pdf?ASCLD-FRC-presentation.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${ascld}</a></p>`;
 });
--- src/pages/Presentations.f1jwj.js
+++ src/pages/Presentations.f1jwj.js
@@ -2,9 +2,50 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    // Write your JavaScript here
+    const colorBlue = "#87aade";
 
-    // To select an element by ID use: $w('#elementID')
+    const part1 = $w("#part1").text;
+    $w("#part1").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_dee4af5a316b4f0484ad5863799dceb9.pdf?part-1.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${part1}</a></p>`;
 
-    // Click 'Preview' to run your code
+    const part2 = $w("#part2").text;
+    $w("#part2").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_973f7feea26d4f8695012246c3d68a11.pdf?part-2.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${part2}</a></p>`;
+
+    const part3 = $w("#part3").text;
+    $w("#part3").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_e5674836c14c40178db1a3291a3ce4b6.pdf?part-3.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${part3}</a></p>`;
+
+    const virtual_crime = $w("#virtual-crime").text;
+    $w("#virtual-crime").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_32e4ca947f644859bd0d6403fbf87250.pdf?COFSE-Webinar-Series.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${virtual_crime}</a></p>`;
+
+    const workshop = $w("#workshop").text;
+    $w("#workshop").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_4b0c42015f454315a34eac92f40a1781.pdf?2021-COFSE-workshop-summary-information-with-abstracts.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${workshop}</a></p>`;
+
+    const rippy = $w("#rippy").text;
+    $w("#rippy").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_206c79cf48b74170aab6c09b00a6a250.pdf?the-use-of-large-scale-faux-crime-scenes-Rippy-AAFS_2019.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${rippy}</a></p>`;
+
+    const seferyn = $w("#seferyn").text;
+    $w("#seferyn").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_ef84a5da13a54de786cfd65d1c4d744b.pdf?implementation-of-the-NIJ-online-firearms-examiner-training-course-in-a-graduate-curriculum-Seferyn_AAFS-2016.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${seferyn}</a></p>`;
+
+    const podini = $w("#podini").text;
+    $w("#podini").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_d3cbdcbeb7524ed59779630fb9465892.pdf?course-focused-on-validation-Podini_AAFS-2016.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${podini}</a></p>`;
+
+    const smolar = $w("#smolar").text;
+    $w("#smolar").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_824d6300f46b403d8f84a388119c8cbd.pdf?Designing-an-Interactive-and-Engaging-Online-Forensic-Science-Lab-Course-Londino-Smolar_AAFS-2020.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${smolar}</a></p>`;
+
+    const chadwick = $w("#chadwick").text;
+    $w("#chadwick").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_424c37c8e6f1400ba4293d72778ba1ee.pdf?Forensic-Science-Education-Future-proof-or-Flawed-Paradigm-Chadwick-and-Roux-AAFS_2019.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${chadwick}</a></p>`;
+
+    const conte = $w("#conte").text;
+    $w("#conte").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_0405b5e3900e4eedb7df0558cd8bed18.pdf?A-Collaboration-for-Bridging-the-High-School-to-College-Transition-Conte-and-Hudgins_AAFS-2018.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${conte}</a></p>`;
+
+    const mabry = $w("#mabry").text;
+    $w("#mabry").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_29ed447553074ae2aceffea04875bfc0.pdf?An-Interdisciplinary-Approach-to-Forensic-Science-Eduction-Mabry_AAFS-2016.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${mabry}</a></p>`;
+
+    const presley = $w("#presley").text;
+    $w("#presley").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_db0a8199eab7463899ef0c7fc8ff12d1.pdf?Strengthening-Forensic-Science-Education-through-COFSE-Presley_2010.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${presley}</a></p>`;
+
+    const kobojek = $w("#kobojek").text;
+    $w("#kobojek").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_bc4b2dcdebe24c8a8f8ebab0c7145019.pdf?Addressing-the-Challenge-of-Undergraduate-Research-in-Forensic-Science-Kobojek__AAFS-2019.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${kobojek}</a></p>`;
+
+    const brodeur = $w("#brodeur").text;
+    $w("#brodeur").html = `<p style="text-align: center;"><a href="https://fbf59b14-1a92-4ba9-a7e6-89c702f37f2b.usrfiles.com/ugd/fbf59b_1b3e8fa4d1124bb4b3cd14a0d8e37112.pdf?Collaboration-with-LEAP-Brodeur_NEAFS-2018.pdf" target="_blank" style="color: ${colorBlue}; font-weight: bold;">${brodeur}</a></p>`;
 });
