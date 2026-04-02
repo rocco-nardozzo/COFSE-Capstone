c895faba6947ada6b8fdd510ac94b1ad9b2714dd
Rocco Nardozzo <nardozzor@duq.edu>
2026-04-01 23:22:15 -0400

    Improve members only functionality

--- src/pages/Annual Meetings.vt30q.js
+++ src/pages/Annual Meetings.vt30q.js
@@ -1,7 +1,6 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
-$w.onReady(function () {
+
+$w.onReady(async function () {
 
     const colorBlue = "#87aade";
 
